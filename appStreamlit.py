0af37da126a922293777d3eb50927f0c7291d93b
Chris Orear <chor8354@colorado.edu>
2024-02-05 14:41:17 -0700

    streamlit_cropper changes

--- appStreamlit.py
+++ appStreamlit.py
@@ -1,39 +1,31 @@
-import streamlit as st
-from PIL import Image
+
+from PIL import Image, ImageDraw
 import cv2
 import matplotlib.pyplot as plt
-import mpld3
-import streamlit.components as components
 
-def imageupload():
-    st.title("Image Upload")
-    uploaded_files = st.file_uploader("Choose an image...", type=["jpg","jpeg","png"], accept_multiple_files = True)
-    if uploaded_files is not None:
-            for image in uploaded_files:
-                img = Image.open(image)
-                fig, ax = plt.subplots()
-                ax.imshow(img)
-                ax.set_title("Interactive Matplotlib Figure")
-                ax.set_axis_off()  # Turn off axis for better appearance
-            return fig
 
-def plot_image():
-    fig, ax = plt.subplots()
-    ax.imshow()
-    ax.set_title("Interactive Matplotlib Figure")
-    ax.set_axis_off()  # Turn off axis for better appearance
-    return fig
+
+
+import streamlit as st
+from streamlit_cropper import st_cropper
 
 # Define UI for image upload and display
 def app():
     st.title("GVA-holes")
-    fig = imageupload()
-    # Display the image using Matplotlib in Streamlit
-    #fig = plot_image()
-    # Display the Matplotlib figure in the Streamlit app
-    st.pyplot(fig)
-    fig_html = mpld3.fig_to_html(fig)
-    components.html(fig_html, height=600)
+
+# Upload image through Streamlit
+uploaded_images = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"], accept_multiple_files= True)
+if uploaded_images is not None:
+    for uploaded_image in uploaded_images:
+        image = Image.open(uploaded_image)
+    # Display the original image
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+    # Define a placeholder for cropped image
+        cropped_image = None
+    # Use st_cropper for interactive cropping
+        cropped_image = st_cropper(image, box_color="red")
+    # Display the cropped image
+        st.image(cropped_image, caption="Cropped Image", use_column_width=True)
 
 if __name__ == '__main__':
     app()
