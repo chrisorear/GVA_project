57ee44651419a7307b5e05f08faae965c42c94c1
Chris Orear <chor8354@colorado.edu>
2024-02-05 16:21:12 -0700

    added drawing tool (AR wrong)

--- appStreamlit.py
+++ appStreamlit.py
@@ -4,13 +4,16 @@ import cv2
 import matplotlib.pyplot as plt
 import streamlit as st
 from streamlit_cropper import st_cropper
+from streamlit_drawable_canvas import st_canvas
 
 # Define UI for image upload and display
 def app():
     st.title("GVA-holes")
 
+#configure webpage
 st.set_page_config(page_title="GVA-holes", page_icon="🖼️")
 
+#all functions are here
 #function to adjust image brightness and contrast to increase clarity of colonies
 def adjust_image(uploaded_image):
     pil_image = Image.open(uploaded_image)
@@ -38,13 +41,55 @@ if uploaded_images is not None:
         cropped_image_name2 = "Middle Pipette" + " " + file_name
         cropped_image_name3 = "Bottom Pipette" + " " + file_name
          # Use st_cropper for interactive cropping
-        cropped_image1 = st_cropper(adjusted_image, box_color="red")
-        cropped_image2 = st_cropper(adjusted_image, box_color="blue")
-        cropped_image3 = st_cropper(adjusted_image, box_color ="green")
+        st.text("Please draw a red box around the top pipette in the image below, then double click to crop.")
+        cropped_image1 = st_cropper(adjusted_image, box_color="red", realtime_update=False)
+        st.text("Please draw a blue box around the middle pipette in the image below, then double click to crop..")
+        cropped_image2 = st_cropper(adjusted_image, box_color="blue", realtime_update= False)
+        st.text("Please draw a green box around the bottom pipette in the image below, then double click to crop..")
+        cropped_image3 = st_cropper(adjusted_image, box_color ="green", realtime_update=False)
     # Display the cropped image
-        st.image(cropped_image1, caption=cropped_image_name1, use_column_width=True)
-        st.image(cropped_image2, caption=cropped_image_name2, use_column_width=True)
-        st.image(cropped_image3, caption=cropped_image_name3, use_column_width=True)
+        #cropped_image_1 = st.image(cropped_image1, caption=cropped_image_name1, use_column_width=True)
+        #cropped_image_2 = st.image(cropped_image2, caption=cropped_image_name2, use_column_width=True)
+        #cropped_image_3 = st.image(cropped_image3, caption=cropped_image_name3, use_column_width=True)
+
+#Code to make the cropped images drawable
+# Initialize the canvas
+        # Specify canvas parameters in application
+        drawing_mode = st.sidebar.selectbox(
+            "Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform")
+        )
+
+        stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+        point_display_radius = 0
+        stroke_color = ""
+        bg_color = ""
+        bg_image = cropped_image1
+        realtime_update = st.sidebar.checkbox("Update in realtime", True)
+
+        if drawing_mode == 'point':
+            point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
+            stroke_color = st.sidebar.color_picker("Stroke color hex: ")
+            bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
+
+        if drawing_mode == 'line':
+            line_display_radius = st.sidebar.slider("Line display radius: ", 1, 25, 3)
+            stroke_color = st.sidebar.color_picker("Stroke color hex: ")
+            bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
+
+        width1, height1 = cropped_image1.size
+
+        # Create a canvas component
+        canvas_result = st_canvas(
+            fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+            stroke_width=stroke_width,
+            stroke_color=stroke_color,
+            background_color=bg_color,
+            background_image=bg_image,
+            update_streamlit=realtime_update,
+            drawing_mode=drawing_mode,
+            point_display_radius=point_display_radius,
+            key="canvas",
+        )
 
 if __name__ == '__main__':
     app()
