a18f00f8a2304f5718ef7301dfeeedbb13a4490f
Chris Orear <chor8354@colorado.edu>
2024-02-21 10:48:18 -0700

    argudit changes GVA calcs

--- GVAWebsite.py
+++ GVAWebsite.py
@@ -9,7 +9,8 @@ import mpld3
 import streamlit.components as components
 from streamlit_drawable_canvas import st_canvas
 from streamlit_cropper import st_cropper
-import os
+import plotly.graph_objects as go
+
 
 #Put instructions for the user
 instructions1 = "Welcome to the GVA-holes website. If you are reading this, you're probably a super cool person that loves to calculate bacterial concentrations, in other words, you're a GVA-hole. If so, please follow the instructions below:"
@@ -17,7 +18,28 @@ instructions2 = "First, you'll upload your image. It will show up three times. O
 
 #configure webpage
 st.set_page_config(page_title="GVA-holes", page_icon="🆘", layout="wide")
+# Specify what pages should be shown in the sidebar, and what their titles and icons
+# should be
+def main_page():
+    st.markdown("# Main page 🎈")
+    st.sidebar.markdown("# Main page 🎈")
 
+def page2():
+    st.markdown("# Page 2 ❄️")
+    st.sidebar.markdown("# Page 2 ❄️")
+
+def page3():
+    st.markdown("# Page 3 🎉")
+    st.sidebar.markdown("# Page 3 🎉")
+
+page_names_to_funcs = {
+    "Main Page": main_page,
+    "Page 2": page2,
+    "Page 3": page3,
+}
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
 
 #all functions are here
 #function to adjust image brightness and contrast to increase clarity of colonies
@@ -32,6 +54,7 @@ def adjust_image(uploaded_image):
 #Code to make the cropped images drawable
 # Initialize the canvas
         # Specify canvas parameters in application
+
 
 def canvas(cropped_image):
     drawing_mode = "point"
@@ -62,6 +85,7 @@ def canvas(cropped_image):
 # Upload image through Streamlit
 uploaded_images = st.file_uploader("Upload image files here", type=["jpg", "jpeg", "png"], accept_multiple_files= True)
 
+
 if uploaded_images is not None:
     for uploaded_image in uploaded_images:
         file_name = uploaded_image.name
@@ -72,25 +96,33 @@ if uploaded_images is not None:
         width1, height1 = adjusted_image.size
         #box_coords = (0, width1, 2/3*height1, height1)
         cropped_images = []
-
+        j = 0
         for i in range(num_regions):
             st.subheader(f"Crop Region {i+1}")
             box_coords = (0, width1, (i)/3*height1, (i+1)/3*height1)
             # Perform cropping with a unique key for each st_cropper widget
             cropped_images.append(st_cropper(adjusted_image, default_coords = box_coords, key=f"cropper_{i}"))
-            st.write("Cropping Box Coordinates:", box_coords)
             # Display the cropped image
-            canvas(cropped_images[i])
-            
-            
-            #broken code - doesn't run but maybe a good start point for saving resulting cropped images w drawings to a folder on computer
-            image_data = canvas_result1._st_image_data
-            directory_path = os.path.dirname(file_name)
-            file_path = os.path.join(directory_path, f"region_{i}")
-            if image_data is not None:
-                with open("canvas_image.png", "wb") as f:
-                    f.write(image_data)
-
-
-
-#py -m streamlit run "C:\Users\argud\OneDrive\Desktop\Spring 2024\Senior Design\Website Code\GVAWebsite.py"
+            drawable = canvas(cropped_images[i])
+            if drawable.json_data is not None:
+                colonies = []
+                objects = pd.json_normalize(drawable.json_data["objects"]) # need to convert obj to str because PyArrow
+                for col in objects.select_dtypes(include=['object']).columns:
+                    left = objects["left"].astype("double")
+                    colonies = left.tolist()
+                #Finds length of pipette tip in pixels
+                if len(colonies) >= 2:
+                    h = np.max(colonies) - np.min(colonies)
+                    convertH = 36/h
+                    max = np.max
+                    #Removes min and mix values because they are not colonies
+                    colonies.remove(max(colonies))
+                    colonies.remove(min(colonies))
+                    if len(colonies) >= 1:
+                        st.write("Pipette Length in pixels", h)
+                        st.write("Colony Locations", colonies)
+                        x2 = np.min(colonies)*convertH
+                        st.write("x2",x2)
+                        x1 = np.max(colonies)*convertH
+                        CFUs = len(colonies)/(np.absolute((np.power(x2,3) - np.power(x1,3)))/(3*36^2)*np.pi*1.995)
+                        st.write("CFUs/mL",CFUs)
