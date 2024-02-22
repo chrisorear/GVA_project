2ee23426d8b6d1567720eed1a666c6bd5ffa2e09
Chris Orear <chor8354@colorado.edu>
2024-02-22 09:29:09 -0700

    playing with session states - doesn't work

--- appStreamlit.py
+++ appStreamlit.py
@@ -9,16 +9,9 @@ import mpld3
 import streamlit.components as components
 from streamlit_drawable_canvas import st_canvas
 from streamlit_cropper import st_cropper
-import os
-
-#Put instructions for the user
-instructions1 = "Welcome to the GVA-holes website. If you are reading this, you're probably a super cool person that loves to calculate bacterial concentrations, in other words, you're a GVA-hole. If so, please follow the instructions below:"
-instructions2 = "First, you'll upload your image. It will show up three times. On the first one, move the red crop box so that it is fully around the top pipette. On the second one, move the blue crop box so that it is fully around the middle pipette in the image. On the third, move the green crop box so that it is fully around the bottom pipette in the image."
-
+import plotly.graph_objects as go
 #configure webpage
 st.set_page_config(page_title="GVA-holes", page_icon="🆘", layout="wide")
-
-
 #all functions are here
 #function to adjust image brightness and contrast to increase clarity of colonies
 def adjust_image(uploaded_image):
@@ -28,16 +21,12 @@ def adjust_image(uploaded_image):
     enhancer = ImageEnhance.Brightness(pil_image)
     pil_image = enhancer.enhance(1.5)
     return pil_image
-
-#Code to make the cropped images drawable
-# Initialize the canvas
-        # Specify canvas parameters in application
-
+#function to make the cropped images drawable
 def canvas(cropped_image):
     drawing_mode = "point"
     width1, height1 = cropped_image.size
     stroke_width = 1
-    point_display_radius = 2
+    point_display_radius = 0.5
     stroke_color = "black"
     bg_color = ""
     aspect_ratio1 = width1 / height1
@@ -55,53 +44,80 @@ def canvas(cropped_image):
         display_toolbar=True,
         height = 700 / aspect_ratio1,
         width = 700,
-        key = f"image_{file_name}"
+        key = f"file_name, {i}"
         )
     return canvas_result1
+#function to crop images into 3 separate things
+def image_cropper(num_regions):
+    for i in range(num_regions):
+        st.subheader(f"Crop Region {i+1}")
+        box_coords = (0, width1, (i)/3*height1, (i+1)/3*height1)
+        # Perform cropping with a unique key for each st_cropper widget
+        cropped_images.append(st_cropper(adjusted_image, default_coords = box_coords, key=f"cropper_{i}"))
+    return cropped_images   
+#function to pull colony location from canvas as you click on colonies
+def colonyfunc(drawable):            
+    if drawable.json_data is not None:
+        colonies = []
+        objects = pd.json_normalize(drawable.json_data["objects"]) # need to convert obj to str because PyArrow
+        for col in objects.select_dtypes(include=['object']).columns:
+            left = objects["left"].astype("double")
+            colonies = left.tolist()
+            return colonies           
+#function to do GVA calculations from the clicked colony data
+def GVAcalc(colonies):
+    h = np.max(colonies) - np.min(colonies)
+    convertH = 36/h
+    max = np.max
+    #Removes min and mix values because they are not colonies
+    colonies.remove(max(colonies))
+    colonies.remove(min(colonies))
+    if len(colonies) >= 1:
+        st.write("Pipette Length in pixels", h)
+        st.write("Colony Locations", colonies)
+        x2 = np.min(colonies)*convertH
+        x1 = np.max(colonies)*convertH
+        CFUs = len(colonies)/(np.absolute((np.power(x2,3) - np.power(x1,3)))/(1000*(3*36^2)*np.pi*1.995))
+        st.write("CFUs/mL",CFUs)
+# Initialize session state
+def init_session_state():
+    if "current_image_idx" not in st.session_state:
+        st.session_state.current_image_idx = 0
+
 
 # Upload image through Streamlit
-uploaded_images = st.file_uploader("Upload image files here", type=["jpg", "jpeg", "png"], accept_multiple_files= True)
+uploaded_images = st.file_uploader("Upload image files here", type=["jpg", "jpeg", "png"], accept_multiple_files=True, key=session_id)
 
-if 'uploaded_images' not in st.session_state:
-    st.session_state.uploaded_images = []
-#run code for uploaded images
+
+# Initialize session state
+init_session_state()
+# Retrieve or initialize image index from session state
+current_image_idx = session.state.get("current_image_idx", 0)
+# Retrieve current image index from session state
+current_image_idx = st.session_state.current_image_idx
+
 if uploaded_images is not None:
-    for uploaded_image in uploaded_images:
-        file_name = uploaded_image.name
-
-        #open and adjust image
-        adjusted_image = adjust_image(uploaded_image)
-        #initialize new session state for each image
-        st.session_state.uploaded_images.append(adjusted_image)
-        #define number of "crop regions" and find the width and height of the image in pixels
-        width1, height1 = adjusted_image.size
-        cropped_images = []
-
-        if st.sidebar.button(file_name, key = f"File + {file_name}"):
-            st.title(f"Image {file_name}")
-            num_regions = st.number_input("Number of regions to crop:", min_value=1, max_value=10, value=3, key = file_name)
-            #for number of crop regions, crop and display on drawable canvas using canvas function
-            for j in range(num_regions):
-                st.subheader(f"Crop Region {j+1}")
-                box_coords = (0, width1, (j)/3*height1, (j+1)/3*height1)
-                    # Perform cropping with a unique key for each st_cropper widget
-                cropped_images.append(st_cropper(adjusted_image, default_coords = box_coords, key=f"cropper_{j}"))
-                st.write("Cropping Box Coordinates:", box_coords)
-                # Display the cropped image
-                img = canvas(cropped_images[j])
-                
-        
-
-       
-            
-            #broken code - doesn't run but maybe a good start point for saving resulting cropped images w drawings to a folder on computer
-            #image_data = img._st_image_data()
-            #directory_path = os.path.dirname(file_name)
-            #file_path = os.path.join(directory_path, f"region_{i}")
-            #if image_data is not None:
-                #with open("canvas_image.png", "wb") as f:
-                    #f.write(image_data)
-
-
-
-#py -m streamlit run "C:\Users\argud\OneDrive\Desktop\Spring 2024\Senior Design\Website Code\GVAWebsite.py"
+    # Display images one by one
+    uploaded_image = uploaded_images[current_image_idx]
+    st.subheader(f"Image {current_image_idx + 1}/{len(uploaded_images)}")
+    file_name = uploaded_image.name
+    # Open and adjust image
+    adjusted_image = adjust_image(uploaded_image)
+    num_regions = st.number_input("Number of regions to crop:", min_value=1, max_value=10, value=3)
+    width1, height1 = adjusted_image.size
+    cropped_images = []
+    cropped_images = image_cropper(num_regions)
+    for i, image in enumerate(cropped_images):
+        drawable = canvas(image)
+        colonies = colonyfunc(drawable)
+        st.write(colonies)
+        if colonies is not None:
+            GVAcalc(colonies)
+    
+    # Add navigation buttons
+    if current_image_idx > 0:
+        if st.button("Previous"):
+            st.session_state.current_image_idx -= 1
+    if current_image_idx < len(uploaded_images) - 1:
+        if st.button("Next"):
+            st.session_state.current_image_idx += 1
